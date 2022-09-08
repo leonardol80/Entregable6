a923c8364de6b9cf031b55084ae648439c82a658
leonardol80 <leonardolayam@gmail.com>
2022-09-08 13:20:37 -0400

    Agregado Purchases

--- src/components/routes/Purchases.jsx
+++ src/components/routes/Purchases.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import getConfig from '../../utils/getConfig'
+import PurchasesCard from '../purchases/PurchasesCard'
 
 const Purchases = () => {
+
+const [purchases, setPurchases] = useState()
+
+useEffect(()=>{
+  const URL='https://ecommerce-api-react.herokuapp.com/api/v1/purchases'
+  axios.get(URL,getConfig())
+  .then(res => setPurchases(res.data.data.purchases))
+  .catch(err => console.log(err))
+},[])
+
+// console.log(purchases)
+
   return (
-    <div>Purchases</div>
+    <div className='purchase'>
+      <h2 className='purchase__title'>Purchases</h2>
+      <div className='purchase__container'></div>
+    {
+      purchases?.map(purchase =>(
+        <PurchasesCard 
+        key={purchase.id}
+        purchase ={purchase}
+      />
+      ))
+    }
+    </div>
   )
 }
 
